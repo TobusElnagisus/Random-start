5442ec5da9796de15c3f3682475e3e400f32bd67
TobusElnagisus <tobyelnagarigregory@gmail.com>
2026-08-05 10:23:40 +0100

    Change script execution from sh to bash

--- rand-start.py
+++ rand-start.py
@@ -4,24 +4,24 @@ from random import choice
 rand = choice(list(range(1, 11)))
 
 if rand == 1:
-    system("sh ~/.ran/1.sh")
+    system("bash ~/.ran/1.sh")
 elif rand == 2:
-    system("sh ~/.ran/2.sh")
+    system("bash ~/.ran/2.sh")
 elif rand == 3:
-    system("sh ~/.ran/3.sh")
+    system("bash ~/.ran/3.sh")
 elif rand == 4:
-    system("sh ~/.ran/4.sh")
+    system("bash ~/.ran/4.sh")
 elif rand == 5:
-    system("sh ~/.ran/5.sh")
+    system("bash ~/.ran/5.sh")
 elif rand == 6:
-    system("sh ~/.ran/6.sh")
+    system("bash ~/.ran/6.sh")
 elif rand == 7:
-    system("sh ~/.ran/7.sh")
+    system("bash ~/.ran/7.sh")
 elif rand == 8:
-    system("sh ~/.ran/8.sh")
+    system("bash ~/.ran/8.sh")
 elif rand == 9:
-    system("sh ~/.ran/9.sh")
+    system("bash ~/.ran/9.sh")
 elif rand == 10:
-    system("sh ~/.ran/10.sh")
+    system("bash ~/.ran/10.sh")
 else:
     print("FUCK, THERES AN ERROR")
