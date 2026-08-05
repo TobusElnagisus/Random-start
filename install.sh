a48d8e43c917cf37b0f279ff9ec6a551dc30407f
TobusElnagisus <tobyelnagarigregory@gmail.com>
2026-08-05 11:04:43 +0100

    Fix path for moving rand-start.py and cleanup

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ mkdir .ran
 cd .ran
 touch 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh 9.sh 10.sh 
 cd
-mv ~/Random-start/rand-start.py +/.ran
-rm -rf Random-start
+mv ~/Random-start/rand-start.py ~/.ran
+rm -rf ~/Random-start
